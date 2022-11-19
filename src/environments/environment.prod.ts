511156f2866f2def635cc585cf7fbbdf833054a3
rajibganguly <onezerodesigns@gmail.com>
2022-11-19 22:39:59 +0530

    add image

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
   dashboardCardDetails: './assets/data/dashboard-carddetails.json',
   projectsDetails: './assets/data/projects-details.json',
   introDetails: './assets/data/intro-banner.json',
+  noImg: 'https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg',
   companies: {
     barclays: './assets/data/companies/barclays.json',
     hsbc: './assets/data/companies/hsbc.json',
